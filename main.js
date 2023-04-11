3431b7f1a854d1ef93c0c1a42f1fb7e0af5afb03
Prow3 <whchdjfncbncnfndj@gmail.com>
2023-04-11 11:44:02 +0300

    new file:   .gitignore 	new file:   index.html 	new file:   main.js 	new file:   package-lock.json 	new file:   package.json 	new file:   server.js

---
+++ main.js
@@ -0,0 +1,16 @@
+// const button = document.getElementById("button");
+// function post() {
+//   let info = {
+//     name: document.getElementById("name").value,
+//     surname: document.getElementById("surname").value,
+//     email: document.getElementById("email").value,
+//   };
+//   fetch("/registration", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(info),
+//   });
+// }
+// button.addEventListener("click", post);
---
+++ server.js
@@ -0,0 +1,9 @@
+// import express from "express";
+// let app = express();
+// app.use(express.static("."));
+// app.use(express.json());
+// app.post("/registration", (req, res) => {
+//   console.log(req.body);
+// });
+
+// app.listen(3000);
